068edb923fb34eee3730b17fa0e7faf4312b4aba
raicdhel <chj1338@gmail.com>
2020-11-06 00:49:09 +0900

    second commit

--- src/main/java/pizza/external/PaymentService.java
+++ src/main/java/pizza/external/PaymentService.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-//@FeignClient(name="payment", url="http://payment:8080")
-@FeignClient(name="payment", url="http://localhost:8082")
+@FeignClient(name="payment", url="http://payment:8080")
+//@FeignClient(name="payment", url="http://localhost:8082")
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")
